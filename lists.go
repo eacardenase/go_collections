a03ac9dff456ec5091305869308ffbe72a601eac
Edwin Cardenas <eacardenase@gmail.com>
2024-11-10 17:28:49 -0500

    feat: array alternate syntax and indexing

--- lists.go
+++ lists.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	var bookNames [4]string = [4]string{"The Martian", "Project Hail Mary", "The Three Body Problem"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 
+	bookNames[3] = "Cibola Burn"
+
 	fmt.Println(prices)
+	fmt.Println(bookNames)
+
+	fmt.Println(prices[2])
 }
