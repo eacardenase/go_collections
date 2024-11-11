a7923b4d3329e0f9d0b2eb46ec81975defd5b925
Edwin Cardenas <eacardenase@gmail.com>
2024-11-11 15:12:35 -0500

    feat: maps introduction

---
+++ maps.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"gcp": "https://cloud.google.com",
+		"aws": "https://aws.com",
+	}
+
+	fmt.Println(websites)
+	fmt.Println(websites["aws"])
+
+	websites["azure"] = "https://portal.azure.com"
+	fmt.Println(websites)
+
+	delete(websites, "gcp")
+	fmt.Println(websites)
+}
